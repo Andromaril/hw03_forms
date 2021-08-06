e0c1c6b0604455ee8560f5201a67fd88e1a9d3c0
Andronik Maria <andronikmar@yandex.com>
2021-08-06 22:28:48 +0300

    Исправление5

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -6,12 +6,3 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group',)
-
-    def clean_subject(self):
-        text = self.cleaned_data['text']
-        group = self.cleaned_data['group']
-
-        if text == "":
-            raise forms.ValidationError('Без текста пост нельзя создать.')
-
-        return text
